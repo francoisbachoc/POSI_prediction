f944c4bb3ade297bf2913359ca6b5df5ddb0be32
francoisbachoc <francois.bachoc@gmail.com>
2023-02-07 14:17:02 +0100

    Add files via upload

---
+++ exchangeable_protected_intercept_p9_n20_SCAD_1_withRes/run3.R
@@ -0,0 +1,75 @@
+rm( list=ls() )   #equivalent de clear scilab
+
+
+####################################################
+#loading of the settings
+####################################################
+load("settings.Rdata")
+source("../functions.r")
+set.seed(1)
+
+
+
+######################################################
+#execution
+####################################################
+
+#execution time
+tStart=Sys.time()
+
+
+#preparation of the constants
+cat("########### \n")
+cat("preparation of the constants \n")
+cat("########### \n")
+r=n-p
+K = matrixKthreeInf(x0,Xc,r,alpha,I,J)
+Ktest = matrixKthreeOld(x0,Xc,r,alpha,I,J)
+if (max(abs(K-Ktest)) > 0.1) {
+  cat("max difference two versions K3: ",max(abs(K-Ktest)),"\n")
+}
+
+#choice of the target
+target=1 #BetaMn
+
+#minCoverage probability
+res=minCoverageProbability(nbeta1,nbeta2,nbatch3,nmc1,nmc2,nmc3,x0,X,Xc,sigma,Sigma,K,target,modelSelection,argModelSelection)
+mbeta1 = res$mbeta1
+mbeta2 = res$mbeta2
+beta3 = res$beta3
+vCov1 = res$vCov1
+vCov2 = res$vCov2
+vCov3 = res$vCov3
+vHLCI1 = res$vHLCI1
+vHLCI2 = res$vHLCI2
+vHLCI3 = res$vHLCI3
+vModSize1 = res$vModSize1
+vModSize2 = res$vModSize2
+vModSize3 = res$vModSize3
+minCovP = res$minCovP
+
+
+#the total execution time
+tExec=Sys.time() - tStart
+
+
+
+##############################################################
+#Save of the result
+##############################################################
+save(tExec,mbeta1,mbeta2,beta3,vCov1,vCov2,vCov3,vHLCI1,vHLCI2,vHLCI3,vModSize1,vModSize2,vModSize3,minCovP, file = "res3.Rdata" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
